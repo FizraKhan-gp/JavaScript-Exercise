0f752a77405e444eec41003466b4de7fb11f6f0d
FizraKhangp <fizzrakhangp@gmail.com>
2026-09-11 17:05:11 +0500

    clne

---
+++ Chap-12to16/app.js
@@ -0,0 +1,58 @@
+// 1. Declare an empty array using JS literal notation to store student names in future.
+
+var studentName =[]
+
+// 2. Declare an empty array using JS object notation to store student names in future.
+
+var newarray = new Array ()
+
+// 3. Declare and initialize a strings array.
+
+var  stringsArray   =["SSC", "HSC", "BCS",
+    "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+
+// 4. Declare and initialize a numbers array.
+
+var numbersArray   =[10 , 20 , 30 , 40 ,50 , 60 ,70 ,80]
+
+// 5. Declare and initialize a boolean array.
+
+var booleanArray = [true, false];
+
+// 6. Declare and initialize a mixed array.
+
+var mixedArray = ["Umra", 25 , true ,]
+
+// 7. Declare and Initialize an array and store available education qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). Show the listedqualifications in your browser like:
+
+document.write( `<h1>Qualification</h1><br>1)${stringsArray[0]} <br>2)${stringsArray[1]}<br>3)${stringsArray[2]}<br>$4){stringsArray[3]}<br>5)${stringsArray[4]}<br>6)${stringsArray[5]}<br>7)${stringsArray[6]}<br>8)${stringsArray[7]} <br>`)
+
+// 8. Write a program to store 3 student names in an array.Take another array to store score of these three students. Assume that total marks are 500 for each student, display the scores & percentages of students like:
+
+var student= ["Michael", "John", "Tony"]
+var score = [320, 230,480]
+document.write(`Score of ${student[0]} is ${score[0]}. Percentage ${(score[0]/500) *100 }% <br>`)
+document.write(`Score of ${student[1]} is ${score[1]}. Percentage ${(score[1]/500) *100 }% <br>`)
+document.write(`Score of ${student[2]} is ${score[2]}. Percentage ${(score[2]/500) *100 }% <br>`)
+
+// 9. Initialize an array with color names. Display the array elements in your browser.
+
+// a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array.Display the updated array in your browser.
+
+
+// b. Ask the user what color he/she wants to add to the end & add that color to the end of the array. Display theupdated array in your browser.
+
+
+// c. Add two more color to the beginning of the array.Display the updated array in your browser.
+
+
+// d. Delete the first color in the array. Display the updated array in your browser.
+
+
+// e. Delete the last color in the array. Display the updated array in your browser.
+
+
+// f. Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index. . Display the updated array in your browser.
+
+
+// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined position/index. . Display the updated array in your browser.
